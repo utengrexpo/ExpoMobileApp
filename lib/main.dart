ab1e05b22e5f86afd1a5577fda90962c2f0a4663
SourishW <sourishw@gmail.com>
2023-07-22 15:13:32 -0500

    Added Root Page and color styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expo_mobile_app_2023/style.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const RootPage(),
+      theme: ThemeData.from(colorScheme: colorScheme),
     );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const colorScheme = ColorScheme(
+  brightness: Brightness.light,
+  // colors that follow start with FF because FF indicates the alpha
+  primary: Color(0xFFBF5700),
+  onPrimary: Colors.black,
+  secondary: Color(0xFFF8971F),
+  onSecondary: Colors.black,
+  error: Color(0xFFFF0000),
+  onError: Colors.white,
+  background: Color(0xFFFFEBDC),
+  onBackground: Colors.black,
+  surface: Colors.white,
+  onSurface: Colors.black,
+);
